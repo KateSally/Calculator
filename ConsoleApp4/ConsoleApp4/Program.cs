c1ee255df6b098d7320abb8c3100bc26f60b1e95
Kate <katesally@yandex.ru>
2019-06-07 21:47:24 +0500

    Первый вариант (плюс, минус)

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c = 0;
+            string znak;
+            a = Convert.ToInt32(Console.ReadLine());
+            znak = Console.ReadLine();
+            b = Convert.ToInt32(Console.ReadLine());
+            if (znak == "+")
+                {
+                    c = a + b;
+                }
+            if (znak == "-")
+                {
+                    c = a - b;
+                }
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
